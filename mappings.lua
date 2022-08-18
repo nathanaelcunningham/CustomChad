07afd4bd22c03b0859c1ed6e0343f7c1c54c731c
Nathanael Cunningham <nathanaelcunningham@gmail.com>
2022-08-18 18:58:31 +0000

    fix mappings

--- mappings.lua
+++ mappings.lua
@@ -2,17 +2,37 @@ local M = {}
 
 M.lsp = {
   n = {
-    ["K"] = { "vim.lsp.buf.hover()", "Hover symbol details" },
-    ["<leader>la"] = { "vim.lsp.buf.code_action()", "Code Action" },
-    ["<leader>lf"] = { "vim.lsp.buf.formatting_sync()", "Format Code" },
-    ["<leader>lh"] = { "vim.lsp.buf.signature_help()", "Signature Help" },
+    ["K"] = {
+      function()
+        vim.lsp.buf.hover()
+      end,
+      "Hover symbol details",
+    },
+    ["<leader>la"] = {
+      function()
+        vim.lsp.buf.code_action()
+      end,
+      "Code Action",
+    },
+    ["<leader>lf"] = {
+      function()
+        vim.lsp.buf.formatting_sync()
+      end,
+      "Format Code",
+    },
+    ["<leader>lh"] = {
+      function()
+        vim.lsp.buf.signature_help()
+      end,
+      "Signature Help",
+    },
     ["<leader>lr"] = {
       function()
         require("nvchad_ui.renamer").open()
       end,
       "Rename",
     },
-    ["<leader>ld"] = { "vim.diagnostic.open_float()", "Hover diagnostics" },
+    ["<leader>ld"] = { function() vim.diagnostic.open_float() end, "Hover diagnostics" },
     ["[d"] = {
       function()
         vim.diagnostic.goto_prev()
@@ -25,7 +45,11 @@ M.lsp = {
       end,
       desc = "Next diagnostic",
     },
-    ["<leader>dut"] = {function() require("dapui").toggle() end}
+    ["<leader>dut"] = {
+      function()
+        require("dapui").toggle()
+      end,
+    },
   },
 }
 
